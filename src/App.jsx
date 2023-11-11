fecfa81611daa80a0e3f94b3a7e3ec42783754e9
Aloka Devapriya <28234480+Alokadev@users.noreply.github.com>
2023-11-11 16:50:08 +0530

    Final
    Updated nike_links
    index.css file updated with @apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,19 @@
 import { useState } from "react";
 import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx'
-
-useState
+import { RxDotFilled } from 'react-icons/rx';
+import { slides1 } from "./nike_links";
 
 function App() {
-  const slides = [
-    {
-      url:'https://images.unsplash.com/photo-1637844528612-064026615fcd?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url:'https://images.unsplash.com/photo-1495557077419-2384a0150025?q=80&w=1844&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url:'https://images.unsplash.com/photo-1613070120286-98b11cdb9ae2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url:'https://images.unsplash.com/photo-1552346154-7841f684d259?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url:'https://images.unsplash.com/photo-1577641074792-3c3bd9aad58f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    }  
-  ];
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    const newIndex = isFirstSlide ? slides1.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
+    const isLastSlide = currentIndex === slides1.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
@@ -46,22 +24,20 @@ function App() {
 
   return (
     <div className="max-w-[1000px] h-[600px] w-full m-auto py-16 px-4 relative group">
-      <div style={{backgroundImage: `url(${slides[currentIndex].url})`}}
+      <div style={{backgroundImage: `url(${slides1[currentIndex].url})`}}
       className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
 
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 
-      text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+      <div className="left_arrow">
         <BsChevronCompactLeft onClick={prevSlide} size={30}/>
       </div>
 
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 
-      text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+      <div className="right_arrow">
         <BsChevronCompactRight onClick={nextSlide} size={30}/>
       </div>
       <div className="flex top-4 justify-center py-2">
-        {slides.map((slide, slideIndex) => (
+        {slides1.map((slide, slideIndex) => (
           <div key={slideIndex} onClick={() => goToSlide(slideIndex)}className="text-2xl cursor-pointer">
             <RxDotFilled/>
           </div>
---
+++ src/nike_links/index.js
@@ -0,0 +1,20 @@
+export const slides1 = [
+    {
+      url:'https://images.unsplash.com/photo-1637844528612-064026615fcd?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      url:'https://images.unsplash.com/photo-1495557077419-2384a0150025?q=80&w=1844&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      url:'https://images.unsplash.com/photo-1613070120286-98b11cdb9ae2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      url:'https://images.unsplash.com/photo-1552346154-7841f684d259?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      url:'https://images.unsplash.com/photo-1577641074792-3c3bd9aad58f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      url:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }  
+  ];
